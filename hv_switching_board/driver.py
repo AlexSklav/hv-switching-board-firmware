d4be2fe8db1849db7e877e23175a1e219de8ada4
Ryan Fobel <ryan@fobel.net>
2015-02-02 12:14:15 -0500

    Provide wrappers for bit-level channel accessor/mutator

--- hv_switching_board/driver.py
+++ hv_switching_board/driver.py
@@ -10,11 +10,18 @@ class HVSwitchingBoard(BaseNode):
         BaseNode.__init__(self, proxy, address)
 
     def set_state_of_all_channels(self, state):
+        data = np.array([0]*5, dtype=np.uint8)
+        for i in range(len(state)):
+            data[i / 8] |= (state[i] << i % 8)
         for i in range(5):
-          self.serialize_uint8(state[i])
+            self.serialize_uint8(~data[i])
         self.send_command(CMD_SET_STATE_OF_ALL_CHANNELS)
 
     def state_of_all_channels(self):
         self.data = []
         self.send_command(CMD_GET_STATE_OF_ALL_CHANNELS)
-        return self.data
+        state = np.zeros(40, dtype=np.uint8)
+        for i in range(len(state)):
+            state[i] = self.data[i / 8] & (0x01 << i % 8) == 0
+        return state
+
