7710ed74fb55ff9c98b3782b2e29e2a08802857a
Christian Fobel <christian@fobel.net>
2017-11-22 13:04:16 -0500

    feat(i2c/serial): add software reboot
    Add software reboot support:
    
     - Serial: add `reboot()` command
     - I2C: add `0xA2` command code to trigger reboot

--- src/hv_switching_board.ino
+++ src/hv_switching_board.ino
@@ -4,6 +4,16 @@
 
 #include "HVSwitchingBoard.h"
 
+
+void wdt_init(void) __attribute__((naked)) __attribute__((section(".init3")));
+
+void wdt_init(void) {
+  MCUSR = 0;
+  wdt_disable();
+  return;
+}
+
+
 void setup() {
   // Use 57600 since 115200 does not seem to work on atmega328 running with 8
   // MHz clock.
